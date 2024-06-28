8b662e6493732ce49b29aa0ec472817aab9efc80
kumardevloper <kumardevloper22@gmail.com>
2024-06-28 17:05:04 +0530

    First commit

--- backend/index.js
+++ backend/index.js
@@ -1,37 +1,25 @@
-const express = require("express");
+const express = require("express")
+const mongoose = require("mongoose")
 
-// const URI = "mongodb://127.0.0.1:27017/mymern";
-
-// const connectDb = async () => {
-//     try{
-//         await mongoose.connect(URI);
-//         console.log("Connected to database");
-//     }catch(error){
-//         console.error("database connection failed");
-//         process.exit(0);
-//     }
-// }
-
-//  module.exports = connectDb
-
-
-
-
-
-const mongoose = require("mongoose");
 const app = express()
+mongoose.connect("mongodb://localhost:27017/mymern")
 
-mongoose.connect("mongodb://localhost:27017/mymern",{
-    useNewUrlParser:true,useUnifiedTopology:true
-},(err) => {
-    if(err)
-        {
-            console.log(err)
-        }else{
-            console.log("Connected Successfully")
-        }
+const UserSchema = new mongoose.Schema({
+    name:String,
+    age:Number
 })
 
-app.listen(3000,()=>{
-    console.log("On port 3000 !!")
+const UserModel = mongoose.model("test", UserSchema)
+
+app.get("/getUsers", (req,res) => {
+  UserModel.find({}).then(function(users){
+    res.json(users)
+  }).catch(function(err){
+    console.log(err)
+  })
+})
+
+
+app.listen(3001, () => {
+    console.log("Server is connected")
 })
